8e8985372f3e9cd4390c04f1bdbf2d328ee1520c
SHOMAKHMUDOVICH <shomakhmudovich@mail.ru>
2022-04-03 16:58:00 +0500

    init project

---
+++ src/components/countersList.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import Counter from "./counter";
+
+const CountersList = () => {
+    const initialState = [
+        { id: 0, value: 0, name: "Ненужная вещь", price: "200" },
+        { id: 1, value: 4, name: "Ложка" },
+        { id: 2, value: 0, name: "Вилка" },
+        { id: 3, value: 0, name: "Тарелка" },
+        { id: 4, value: 0, name: "Набор минималиста" },
+    ];
+
+    const [counters, setCounters] = useState(initialState);
+    const handleDelete = (id) => {
+        const newCounters = counters.filter((c) => c.id !== id);
+        setCounters(newCounters);
+    };
+    const handleReset = () => {
+        setCounters(initialState);
+        console.log("handle reset");
+    };
+    const handleIncrement = (id) => {
+        const newCounters = counters.map(c => {if (c.id===id) {c.value++}
+            return c
+        });
+        setCounters(newCounters);
+        
+        
+        
+        
+    };
+    const handleDecrement = (id) => {
+        const newCounters = counters.map(c => {if (c.id===id) {c.value--}
+            return c
+        });
+        setCounters(newCounters);
+        
+    };
+
+    return (
+        <>
+            {counters.map((count) => (
+                <Counter key={count.id} onDelete={handleDelete} onIncrement={handleIncrement} onDecrement={handleDecrement} {...count} />
+            ))}
+            <button
+                className='btn btn-primary btn-sm m-2'
+                onClick={handleReset}
+            >
+                Сброс
+            </button>
+        </>
+    );
+};
+export default CountersList;
